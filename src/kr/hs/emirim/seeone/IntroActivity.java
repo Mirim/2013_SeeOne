d311b706ed9806205e36ecccd7f6e84753ca47e4
SoyeonBaek <bck238@gmail.com>
2013-11-02 13:08:09 +0900

    인트로 완성

--- src/kr/hs/emirim/seeone/IntroActivity.java
+++ src/kr/hs/emirim/seeone/IntroActivity.java
@@ -2,9 +2,11 @@ package kr.hs.emirim.seeone;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -12,10 +14,10 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class IntroActivity extends Activity{
-	ImageView introImage;
 	int value=0;
-	Animation introAnim;
-	
+	AnimationDrawable frameAnimation;
+	ImageView view;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,14 +25,15 @@ public class IntroActivity extends Activity{
 		win.requestFeature(Window.FEATURE_NO_TITLE);
 		win.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.intro);
-		
-		introImage=(ImageView) findViewById(R.id.intro_view);
-		introAnim=AnimationUtils.loadAnimation(IntroActivity.this, R.anim.intro);
-		introImage.startAnimation(introAnim);
-		
+
+		view=(ImageView)findViewById(R.id.imageAnimation);
+		view.setBackgroundResource(R.drawable.introimg);
+		frameAnimation = (AnimationDrawable) view.getBackground();
+
+		frameAnimation.start();
 		mHandler.sendEmptyMessageDelayed(0, 2000);
 	}
-	
+
 	Handler mHandler=new Handler(){
 		public void handleMessage(Message msg){
 			Intent intent=new Intent(IntroActivity.this, MainActivity.class);
